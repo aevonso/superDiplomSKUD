dad797c0481c5a3763028f58d1f614262478b4e5
oddonenso@gmail.com <oddonenso@gmail.com>
2025-04-10 21:39:15 +0700

    realize logic auth(testing) for Asp. SOLID)))

---
+++ serverSKUD/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using AutorizationDomain.Queries.Object;
+using Microsoft.AspNetCore.Mvc;
+using serviceSKUD;
+
+namespace serverSKUD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IQueryService<EntryDto, Employeer?> _authQuery;
+
+        public AuthController(IQueryService<EntryDto, Employeer?> authQuery)
+        {
+            _authQuery = authQuery;
+        }
+
+        [HttpPost("login")]
+        public ActionResult<Employeer> Login([FromBody] EntryDto entryDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = _authQuery.Execute(entryDto);
+            if(result==null)
+            {
+                return Unauthorized("Неверный логин или пароль");
+            }
+            return Ok(result);
+        }
+    }
+}
--- serverSKUD/Program.cs
+++ serverSKUD/Program.cs
@@ -3,6 +3,7 @@ using AutorizationDomain.Queries;
 using AutorizationDomain.Queries.Object;
 using Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using serviceSKUD;
 using System.Security.Claims;
 
@@ -10,22 +11,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-string? path = builder.Configuration.GetConnectionString("pgSql");
-ArgumentNullException.ThrowIfNull(path);
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "СКУД НАТК API",
+        Version = "v1",
+        Description = "API для системы контроля и управления доступом в НАТК"
+    });
+});
+
+// Получение строки подключения из конфигурации
+string? connectionString = builder.Configuration.GetConnectionString("pgSql");
+ArgumentNullException.ThrowIfNull(connectionString);
+
 builder.Services.AddDbContext<Connection>(options =>
-    options.UseNpgsql(path));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddScoped<IQueryService<EntryDto, Employeer?>, AutorizationQueryService>();
 builder.Services.AddScoped<IQueryService<Employeer, ClaimsPrincipal>, CreatePrincipalQueryService>();
 
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "СКУД НАТК API v1");
+    });
 }
 
 app.UseHttpsRedirection();
